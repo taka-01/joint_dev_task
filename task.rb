659950df79cc9d7fe044a6dd05bd4f31ded76936
井上剛嘉 <inouetakahiro@inouetsuyoshiyoshiminoMacBook-Pro.local>
2020-03-18 20:13:54 +0900

    Q7~Q9に回答

--- task.rb
+++ task.rb
@@ -70,8 +70,9 @@ def q7
 
   # 以下に回答を記載
   # p array.map!{|x| x.to_i}
-
-  p array.map &:to_i
+  # p array.map &:to_i
+  
+  p array.map(&:to_i)
 
 end
 
@@ -79,7 +80,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -89,7 +91,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name,i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
