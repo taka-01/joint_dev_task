51364fbdafdf534faff83fd3e55c23f168917831
井上剛嘉 <inouetakahiro@inouetakahironoMacBook-Pro.local>
2020-03-27 23:25:30 +0900

    質問Q18に回答

--- task.rb
+++ task.rb
@@ -237,7 +237,18 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
+  end
 
+  def introduce
+    if @age >= 20
+      "こんにちは、#{@name}と申します。よろしくお願いいたします。"
+    else
+      "はいさいまいど〜、#{@name}です！！！"
+    end
+  end
 end
 
 def q18
